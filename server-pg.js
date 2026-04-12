b3b0e7f5eceb4f264057bef462fab38049a1ba28
snailsstons <mrdrag.666@gmail.com>
2026-04-12 19:02:03 +0200

    Update server-pg.js

--- server-pg.js
+++ server-pg.js
@@ -1,5 +1,5 @@
 // ══════════════════════════════════════════════════════════════════════════════
-// SPOTME SERVER – PostgreSQL Version 1
+// SPOTME SERVER – PostgreSQL Version 2
 //
 // Neu in dieser Version:
 //   • 24h Offline-Sichtbarkeit  → visible_until Timestamp pro Profil
