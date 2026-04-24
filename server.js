d25f1bc7237ad092e254b02d6ed740720f8a972a
snailsstons <mrdrag.666@gmail.com>
2026-04-25 00:13:00 +0200

    Update server.js

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use((req, res, next) => {
 app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 
-// ---------- PeerJS ----------
+// ---------- PeerJS -----------
 const server = require('http').createServer(app);
 const peerServer = ExpressPeerServer(server, {
   path: '/',
