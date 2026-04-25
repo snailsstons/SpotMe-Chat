2417241418b1b88c8427b33c1151cc914f64052d
snailsstons <mrdrag.666@gmail.com>
2026-04-25 10:25:40 +0200

    Update config.js

--- scripts/config.js
+++ scripts/config.js
@@ -11,8 +11,8 @@ const SERVER_PATH = '/peerjs';
 
 // const API_MISSED  = 'https://spotme-pg-test.onrender.com/api/missed-call';
 // const API_BASE    = 'https://spotme-pg-test.onrender.com/api';
-const API_MISSED = 'https://spotme-chat-obom.onrender.com';
-const API_BASE = 'https://spotme-chat-obom.onrender.com';
+const API_MISSED = 'https://spotme-chat-obom.onrender.com/api/missed-call';
+const API_BASE = 'https://spotme-chat-obom.onrender.com/api';
 
 const TOKEN_KEY   = 'sm_token';
 
